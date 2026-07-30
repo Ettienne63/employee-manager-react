485bca64e55ef0f80dad519a4c5cbdcd74ebb4ec
Ettienne Janse van Vuuren <ettienne.jvv@icloud.com>
2026-07-30 14:00:47 +0200

    feat: implement employee creation form

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
+import EmployeeForm from './components/EmployeeForm'
 
 function App() {
+const [employees,setEmployees] = useState([])
 
+function addEmployee(employee){
+  setEmployees(previousEmployees => [
+    ...previousEmployees,
+    employee
+  ])
+}
   return (
     <div>
         <h1>Employee Manager</h1>
+        <EmployeeForm onSave={addEmployee}/>
     </div>
     
   )
---
+++ src/components/EmployeeForm.jsx
@@ -0,0 +1,95 @@
+import { useState } from "react"
+
+function EmployeeForm({onSave}) {
+    const [firstName, setFirstName] = useState("")
+    const [lastName, setLastName] = useState("")
+    const [email, setEmail] = useState("")
+    const [department, setDepartment] = useState("")
+    const [salary, setSalary] = useState("")
+
+    function handleSubmit(event){
+        event.preventDefault();
+
+        const employee = {
+            firstName,
+            lastName,
+            email,
+            department,
+            salary
+        }
+
+        onSave(employee)
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <h2>Add Employee</h2>
+
+            <div>
+                <label>First Name</label>
+                <input
+                    type="text"
+                    value={firstName}
+                    onChange={(event) => {
+                        setFirstName(event.target.value)
+                    }}
+                />
+            </div>
+
+            <div>
+                <label>Last Name</label>
+                <input
+                    type="text"
+                    value={lastName}
+                    onChange={(event) => {
+                        setLastName(event.target.value)
+                    }}
+                />
+            </div>
+
+            <div>
+                <label>Email</label>
+                <input
+                    type="email"
+                    value={email}
+                    onChange={(event) => {
+                        setEmail(event.target.value)
+                    }}
+                />
+            </div>
+
+            <div>
+                <label>Department</label>
+                <select
+                    id="departmentSelect"
+                    value={department}
+                    onChange={(event) => {
+                        setDepartment(event.target.value)
+                    }}>
+                    <option value="">Select Department</option>
+                    <option value="finance">Finance</option>
+                    <option value="admin">Admin</option>
+                    <option value="hr">HR</option>
+                </select>
+            </div>
+
+            <div>
+                <label>Salary</label>
+                <input
+                    type="number"
+                    value={salary}
+                    onChange={(event) => {
+                        setSalary(event.target.value)
+                    }}
+                />
+            </div>
+
+            <div>
+                <button type="submit"> 
+                    Save Employee
+                </button>
+            </div>
+        </form>
+    )
+}
+export default EmployeeForm
